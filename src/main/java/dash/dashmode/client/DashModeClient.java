d2bcefa6a90c49f39e162b46a359c7f7f4425be0
oshi4 <sheglovaoe@gmail.com>
2020-09-30 14:37:44 +0300

    Добавил руды в бумажное измерение

--- src/main/java/dash/dashmode/client/DashModeClient.java
+++ src/main/java/dash/dashmode/client/DashModeClient.java
@@ -5,7 +5,10 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.render.RenderLayer;
+
+import java.awt.*;
 
 @Environment(EnvType.CLIENT)
 public class DashModeClient implements ClientModInitializer {
@@ -13,6 +16,12 @@ public class DashModeClient implements ClientModInitializer {
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(),
                 DashBlocks.PaperFlower, DashBlocks.PaperBirchSapling);
+
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutoutMipped(),
+                DashBlocks.PaperGrass);
+
+        int white = Color.WHITE.getRGB();
+        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> white, DashBlocks.PaperGrass);
     }
 
 }
--- src/main/java/dash/dashmode/registry/DashBlocks.java
+++ src/main/java/dash/dashmode/registry/DashBlocks.java
@@ -13,13 +13,21 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.ToIntFunction;
+
 
 public class DashBlocks {
+    public static final Map<Block, Pair<Integer, Integer>> XpBlocks = new HashMap<>();
+
     public static final Block PaperDirt;
     public static final Block PaperGrass;
     public static final Block PaperStone;
@@ -27,6 +35,14 @@ public class DashBlocks {
     public static final Block PaperLeaves;
     public static final Block PaperBirchSapling;
     public static final Block PaperFlower;
+    public static final Block PaperEmeraldOre;
+    public static final Block PaperQuartzOre;
+    public static final Block PaperGoldOre;
+    public static final Block PaperIronOre;
+    public static final Block PaperCoalOre;
+    public static final Block PaperLapisOre;
+    public static final Block PaperRedstoneOre;
+    public static final Block PaperDiamondOre;
 
     static {
         PaperDirt = new Block(FabricBlockSettings.of(Material.SOIL, MaterialColor.WHITE).strength(0.5F).sounds(BlockSoundGroup.GRAVEL));
@@ -48,6 +64,41 @@ public class DashBlocks {
                 .noCollision()
                 .breakInstantly()
                 .sounds(BlockSoundGroup.GRASS));
+
+        PaperIronOre = new OreBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.WHITE)
+                .requiresTool().strength(3.0F, 3.0F));
+        PaperGoldOre = new OreBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.WHITE)
+                .requiresTool().strength(3.0F, 3.0F));
+
+        PaperLapisOre = new OreBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.WHITE)
+                .requiresTool().strength(3.0F, 3.0F));
+        XpBlocks.put(PaperLapisOre, new Pair<>(2, 5));
+
+        PaperCoalOre = new OreBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.WHITE)
+                .requiresTool().strength(3.0F, 3.0F));
+        XpBlocks.put(PaperLapisOre, new Pair<>(0, 2));
+
+        PaperRedstoneOre = new RedstoneOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.WHITE)
+                .requiresTool()
+                .ticksRandomly()
+                .luminance(createLightLevelFromBlockState(9))
+                .strength(3.0F, 3.0F));
+
+        PaperDiamondOre = new OreBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.WHITE)
+                .requiresTool()
+                .strength(3.0F, 3.0F));
+        XpBlocks.put(PaperDiamondOre, new Pair<>(3, 7));
+
+        PaperEmeraldOre = new OreBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.WHITE)
+                .requiresTool()
+                .strength(3.0F, 3.0F));
+        XpBlocks.put(PaperEmeraldOre, new Pair<>(3, 7));
+
+        PaperQuartzOre = new OreBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.WHITE)
+                .requiresTool()
+                .strength(3.0F, 3.0F)
+                .sounds(BlockSoundGroup.NETHER_ORE));
+        XpBlocks.put(PaperEmeraldOre, new Pair<>(2, 5));
     }
 
     public static void init(String modeName) {
@@ -62,6 +113,14 @@ public class DashBlocks {
         initBlock(new Identifier(modeName, "paper_leaves"), PaperLeaves, defaultSettings);
         initBlock(new Identifier(modeName, "paper_birch_sapling"), PaperBirchSapling, defaultSettings);
         initBlock(new Identifier(modeName, "paper_flower"), PaperFlower, defaultSettings);
+
+        initBlock(new Identifier(modeName, "paper_coal_ore"), PaperCoalOre, defaultSettings);
+        initBlock(new Identifier(modeName, "paper_iron_ore"), PaperIronOre, defaultSettings);
+        initBlock(new Identifier(modeName, "paper_gold_ore"), PaperGoldOre, defaultSettings);
+        initBlock(new Identifier(modeName, "paper_redstone_ore"), PaperRedstoneOre, defaultSettings);
+        initBlock(new Identifier(modeName, "paper_lapis_ore"), PaperLapisOre, defaultSettings);
+        initBlock(new Identifier(modeName, "paper_diamond_ore"), PaperDiamondOre, defaultSettings);
+        initBlock(new Identifier(modeName, "paper_emerald_ore"), PaperEmeraldOre, defaultSettings);
     }
 
     // region Helping methods
@@ -92,6 +151,10 @@ public class DashBlocks {
                         : sideMaterialColor)
                 .strength(2.0F)
                 .sounds(BlockSoundGroup.WOOD));
+    }
+
+    private static ToIntFunction<BlockState> createLightLevelFromBlockState(int litLevel) {
+        return (blockState) -> (Boolean) blockState.get(Properties.LIT) ? litLevel : 0;
     }
 
     // endregion
